e55e62862a3801adef42c46d38611289a049d37d
roy987 <roy.rjacob.jacob@gmail.com>
2025-10-20 19:06:57 +1100

    task: added another interface to meet the min 2 interface requirement

---
+++ Services/IDataService.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp3
+{
+    // Generic interface for data service operations
+    public interface IDataService<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
